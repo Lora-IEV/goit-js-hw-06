3dec6618294ef54121e45946a5d80f03b8391f7b
dymdymch <rachenko.dm@gmail.com>
2022-08-16 15:36:44 +0300

    1-st_step

---
+++ js/task-02.js
@@ -0,0 +1,26 @@
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+
+const ulElement = document.querySelector('ul');
+
+const elements = [];
+
+for (let i = 0; i < ingredients.length; i++){
+  const ingredient = ingredients[i];
+
+  const liElement = document.createElement('li');
+  liElement.textContent = ingredient;
+  liElement.classList.add('item');
+
+  elements.push(liElement); 
+}
+
+ulElement.append(...elements)
+
+console.log(ulElement)
