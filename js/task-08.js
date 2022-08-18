efe94b2aee79511ea0086afdfcf08637e5a27aac
dymdymch <rachenko.dm@gmail.com>
2022-08-18 22:07:43 +0300

    task-6,8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,33 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    
+
+    if (form.email.value === '' || form.password.value === '') {
+        alert('В вас є незаповнене поле!')
+    }
+    
+    else {
+        const formEl = event.currentTarget.elements
+        const login = {
+            mail : formEl.email.value,
+
+            password :  formEl.password.value,
+        }
+    
+
+        console.log(`email : ${login.mail}`);
+        console.log(`password : ${login.password}`)
+
+
+        console.log(' ');
+        console.log(' ');
+        form.reset()
+    } 
+};
